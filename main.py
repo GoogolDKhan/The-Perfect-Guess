fab3c4548c5c1bfda6c53e26b07b76757e90a5a9
Sarfaraz <khansarfaraz2141@gmail.com>
2021-08-22 21:42:00 +0530

    fix: variables name PEP8

--- main.py
+++ main.py
@@ -2,23 +2,23 @@ import random
 
 
 # Generate a random integer between 1 and 100
-randNum = random.randint(1, 100)
+random_number = random.randint(1, 100)
 
 # Initialize guess count and user guess
-guesses = 0
-userGuess = None
+guess_count = 0
+user_guess = None
 
 # Determining the number of guesses user took to correctly guess the generated integer number
-while(userGuess != randNum):
-    userGuess = int(input("Enter your guess between the number 1-100: "))
-    guesses += 1
-    if (userGuess == randNum):
+while(user_guess != random_number):
+    user_guess = int(input("Enter your guess between the number 1-100: "))
+    guess_count += 1
+    if (user_guess == random_number):
         print("You guessed it right!")
     else:
-        if(userGuess > randNum):
+        if(user_guess > random_number):
             print("You guessed it wrong! Enter a smaller number.")
-        elif(userGuess < randNum):
+        elif(user_guess < random_number):
             print("You guessed it wrong! Enter a larger number.")
 
 # Showing the result to the user
-print(f"You guessed the number in {guesses} guesses.")
+print(f"You guessed the number in {guess_count} guesses.")
